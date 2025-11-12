e7c866aac2620b50a7731cda0b7b332dd6ae73de
nanakogit <luckymghein@gmail.com>
2025-11-12 16:03:09 +0630

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-const userID = Deno.env.get('UUID') || 'd342d11e-d424-4583-b36e-524ab1f0afa4'
+const userID = Deno.env.get('UUID') || '90acd4c1-4b9e-4c59-a97f-dc6548982302'
 const proxyIP = Deno.env.get('PROXYIP') || ''
 
 if (!isValidUUID(userID)) {
